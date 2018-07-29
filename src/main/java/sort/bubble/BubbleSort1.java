09a5f4a3bb12135995bc77c3a1acd4c26f14afc9
lilongsheng <lilongsheng@01zhuanche.com>
2018-07-29 23:10:44 +0800

    提交冒泡排序

--- src/main/java/sort/bubble/BubbleSort1.java
+++ src/main/java/sort/bubble/BubbleSort1.java
@@ -1,5 +1,7 @@
 package sort.bubble;
 
+import java.util.Arrays;
+
 /**
  * Created by lilongsheng on 2018/7/29.
  */
@@ -7,5 +9,44 @@ public class BubbleSort1 {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        int[] array = new int[]{6,15,14,7,44,8,6,74,2,4};
+        System.out.println("n="+array.length);
+        int[] arrayResult = BubbleSort(array, array.length);
+        System.out.println(Arrays.toString(arrayResult));
+
+
+//        第9 层循环，结果array ：[6, 14, 7, 15, 8, 6, 44, 2, 4, 74]
+//        第8 层循环，结果array ：[6, 7, 14, 8, 6, 15, 2, 4, 44, 74]
+//        第7 层循环，结果array ：[6, 7, 8, 6, 14, 2, 4, 15, 44, 74]
+//        第6 层循环，结果array ：[6, 7, 6, 8, 2, 4, 14, 15, 44, 74]
+//        第5 层循环，结果array ：[6, 6, 7, 2, 4, 8, 14, 15, 44, 74]
+//        第4 层循环，结果array ：[6, 6, 2, 4, 7, 8, 14, 15, 44, 74]
+//        第3 层循环，结果array ：[6, 2, 4, 6, 7, 8, 14, 15, 44, 74]
+//        第2 层循环，结果array ：[2, 4, 6, 6, 7, 8, 14, 15, 44, 74]
+//        第1 层循环，结果array ：[2, 4, 6, 6, 7, 8, 14, 15, 44, 74]
+//        第0 层循环，结果array ：[2, 4, 6, 6, 7, 8, 14, 15, 44, 74]
+
+//        左下角到右上角的对角线 连接起来，是每次排序后最大元素
+    }
+
+    /**
+     * 冒泡排序  动态图地址 http://www.atool.org/sort.php?rqnglq=9h8ac3
+     * 默认从小到大排序 泡泡每次到最后一个数字 需要注意的是外层循环次数 n 如果是长度那么为n-1 ,边界地方要有等于号
+     * @param array 数字数组
+     * @param n 数组长度
+     * @return 排序后的数组
+     */
+    private static int[] BubbleSort(int[] array,int n){
+        for (int pass = n-1;pass >= 0;pass--){
+            for (int i = 0;i <= pass-1;i++){
+                if (array[i] >= array[i+1]){
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                }
+            }
+            System.out.println("第" +pass+ " 层循环，结果array ："+ Arrays.toString(array));
+        }
+        return array;
     }
 }
