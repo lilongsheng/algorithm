1a88cc646f8810a5aac4c865cc3fe931b55c813d
lilongsheng <lilongsheng@01zhuanche.com>
2018-07-29 21:21:50 +0800

    建立项目结构

---
+++ src/main/java/sort/bubble/BubbleSort1.java
@@ -0,0 +1,11 @@
+package sort.bubble;
+
+/**
+ * Created by lilongsheng on 2018/7/29.
+ */
+public class BubbleSort1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
